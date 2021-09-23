bcf0539c32bdc1886cc001cde9c8836be0440a1d
Ariel Agress <ari@ariels-mac-mini.attlocal.net>
2021-09-22 17:39:59 -0700

    Pulled information for Anime Page

--- src/component/AnimePage.js
+++ src/component/AnimePage.js
@@ -1,6 +1,11 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router';
 import utils from '../utils/animeAPI.js';
+import styled from 'styled-components'
+
+const CoverImage = styled.img`
+
+`
 
 const AnimePage = () => {
     const params = useParams();
@@ -14,15 +19,35 @@ const AnimePage = () => {
     useEffect(() => {
       getAnime();
     });
-    
+
   return (
     <div>
       {anime.attributes && (
-        <h1>
-          {anime.attributes.titles.en
-            ? anime.attributes.titles.en
-            : anime.attributes.titles.en_jp}
-        </h1>
+        <div>
+          <div>
+            <CoverImage src={anime.attributes.coverImage.large} />
+          </div>
+          <h1>
+            {anime.attributes.titles.en
+              ? anime.attributes.titles.en
+              : anime.attributes.titles.en_jp}
+          </h1>
+          <div>
+            {anime.attributes.averageRating}
+            {anime.attributes.popularityRank}
+            {anime.attributes.ratingRank}
+            {anime.attributes.description}
+          </div>
+          <div>
+            Anime Details
+            {anime.attributes.subtype}
+            {anime.attributes.startDate}
+            {anime.attributes.ageRating}
+            {anime.attributes.status}
+            {anime.attributes.episodeLength} min
+            {anime.attributes.showType}
+          </div>
+        </div>
       )}
     </div>
   );
